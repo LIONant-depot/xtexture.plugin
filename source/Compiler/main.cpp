2e7d28656dd3153080cb355e4b584cf14488ccec
LionAnt <nickreal03@gmail.com>
2025-09-01 23:26:44 +0800

    checkin without the static command line

--- source/Compiler/main.cpp
+++ source/Compiler/main.cpp
@@ -7,7 +7,7 @@ int main( int argc, const char* argv[] )
     //
     // This is just for debugging
     //
-    if constexpr (!false)
+    if constexpr (false)
     {
         static const char* pDebugArgs[] = 
         { "TextureCompiler"
